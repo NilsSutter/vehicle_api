9cd65bb5c217c1de821cb8a967371665b4d076bf
Nils Sutter <nilssutter@gmx.de>
2019-07-29 13:17:43 +0200

    create location model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_105646) do
+ActiveRecord::Schema.define(version: 2019_07_29_111711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.float "lat"
+    t.float "lng"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "vehicles", force: :cascade do |t|
     t.datetime "created_at", null: false
