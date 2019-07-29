71816f19be02711996beea88c3145c1ef214ea65
Nils Sutter <nilssutter@gmx.de>
2019-07-29 13:10:48 +0200

    include destroy action && route for vehicles

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::VehiclesController < ApplicationController
   def create
     @vehicle = Vehicle.new
-
     if @vehicle.save
+      # respond with empty body
       head :no_content
     else
       render_error
@@ -10,5 +10,11 @@ class Api::V1::VehiclesController < ApplicationController
   end
 
   def destroy
+    # find vehicle with id
+    @vehicle = Vehicle.find(params[:id])
+    # destroy vehicle
+    @vehicle.destroy
+    # respond with empty body
+    head :no_content
   end
 end
