1089a8b0c33c6e7c2cefa52b6910bdb3489c2f75
Nils Sutter <nilssutter@gmx.de>
2019-07-30 13:04:47 +0200

    calculate distance from the updated location

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -4,6 +4,17 @@ class Api::V1::LocationsController < ApplicationController
     @vehicle = Vehicle.find(params[:vehicle_id])
     @location = Location.new(lat: params[:lat], lng: params[:lng]) #refactor later with strong_params
     @location.vehicle = @vehicle
+    # check if distance < 3.5km
+    distance_to_door2door <= 3.5 ? save_location : @vehicle.locations.delete_all
+  end
+
+  private
+  def distance_to_door2door
+    door2door_office = [52.53, 13.403] #[lat, lng]
+    @location.distance_to(door2door_office).round(2)
+  end
+
+  def save_location
     if @location.save
       head :no_content
     else
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
+  reverse_geocoded_by :lat, :lng
+  after_validation :reverse_geocode
   belongs_to :vehicle
 end
